f26961ef2e18362ba86e7ecd49ec8c36c4accae5
Moises David Ramirez Leterier <moises.ramirez@lan.com>
2017-03-07 11:31:59 -0300

    dojo javascript convert arabian to roman

---
+++ src/dojo.js
@@ -0,0 +1,6 @@
+function toRoman(x){
+	if(x == 1)		
+		return "i";
+	else
+		return "ii";
+}
---
+++ src/test/test.js
@@ -0,0 +1,9 @@
+describe("Test Dojo", function() {
+
+	it("should return i when enter 1", function(){
+		assert.equal("i", toRoman(1));
+	});
+   	it("should return ii when enter 2", function(){
+		assert.equal("ii", toRoman(2));
+	});
+});
